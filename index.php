aa676d1e9da371017f0c8c87378019519f4d2c19
masashiendou <hujyorom@gmail.com>
2020-11-14 07:09:28 +0900

    test

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<htmli lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>JavaScript Basics</title>title>
+</meta>head>
+<body>
+
+	<script src="js/main.js"></script>
+	<script>
+		'use strict';
+	</script>
+</body>
+</head>
+
